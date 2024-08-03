c92a273bda6cad7c3cc0dde34ff7622c8055615b
KOSASIH <kosasihg88@gmail.com>
2024-08-03 09:12:28 +0700

    Create crypto.rs

---
+++ utils/crypto.rs
@@ -0,0 +1,12 @@
+// Cryptographic functions
+pub fn generate_key_pair() -> (Vec<u8>, Vec<u8>) {
+    // Generate ECDSA key pair
+}
+
+pub fn encrypt(data: Vec<u8>, public_key: Vec<u8>) -> Vec<u8> {
+    // Encrypt data using ECIES
+}
+
+pub fn decrypt(data: Vec<u8>, private_key: Vec<u8>) -> Vec<u8> {
+    // Decrypt data using ECIES
+}
