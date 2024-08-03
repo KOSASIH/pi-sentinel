6e4b334f4369d3fe3428a8b7933c754268bfd872
KOSASIH <kosasihg88@gmail.com>
2024-08-03 08:29:10 +0700

    Create config.rs

---
+++ config/config.rs
@@ -0,0 +1,9 @@
+// Configuration file for PiSentinel
+pub struct Config {
+    pub node_id: String,
+    pub node_port: u16,
+    pub consensus_algorithm: String,
+    pub smart_contract_platform: String,
+    pub storage_type: String,
+    pub network_type: String,
+}
