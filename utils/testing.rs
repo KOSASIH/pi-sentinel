d3cf18f115e4d9292643498f3654161e98bb816b
KOSASIH <kosasihg88@gmail.com>
2024-08-03 09:13:26 +0700

    Create testing.rs

---
+++ utils/testing.rs
@@ -0,0 +1,12 @@
+// Testing framework
+pub fn test_consensus() {
+    // Test consensus algorithm
+}
+
+pub fn test_smart_contracts() {
+    // Test smart contract execution
+}
+
+pub fn test_network() {
+    // Test network communication
+}
